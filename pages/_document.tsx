e7f9ef79e5466e4a1f772c68c9e0829a608a3453
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-16 15:22:30 +0700

    feat(ssr): add server-side styling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,6 @@
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import createEmotionServer from "@emotion/server/create-instance";
 import Document, {
   Html,
   Main,
@@ -5,20 +8,49 @@ import Document, {
   Head,
   DocumentContext,
 } from "next/document";
+import React from "react";
 
 class HTMLDocument extends Document {
   static async getInitialProps(context: DocumentContext) {
     const originalRenderPage = context.renderPage;
 
+    const cache = createCache({ key: "css" });
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
     context.renderPage = () => {
       return originalRenderPage({
-        enhanceApp: (App) => App,
+        enhanceApp: (App) => {
+          // eslint-disable-next-line react/display-name
+          return (props) => {
+            return (
+              <CacheProvider value={cache}>
+                <App {...props} />
+              </CacheProvider>
+            );
+          };
+        },
       });
     };
 
     const initialProps = await Document.getInitialProps(context);
+
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => {
+      return (
+        <style
+          data-emotion={`${style.key} ${style.ids.join(" ")}`}
+          key={style.key}
+          dangerouslySetInnerHTML={{ __html: style.css }}
+        />
+      );
+    });
+
     return {
       ...initialProps,
+      styles: [
+        ...emotionStyleTags,
+        ...React.Children.toArray(initialProps.styles),
+      ],
     };
   }
 
