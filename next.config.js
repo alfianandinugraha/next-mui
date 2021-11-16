67f6708f452f7aa1de626b021083430f896d7ba5
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-16 15:41:18 +0700

    feat(config): add comment and default config of assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // Use this option if you want to deploy your static app to github pages
+  // For example, if your repository name is 'mystatic' then the set this option to '/mystatic' 
+  assetPrefix: ''
 }
