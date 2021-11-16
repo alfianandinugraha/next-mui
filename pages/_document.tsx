1ac05979cdb9a4e5d7bacb5a9b19fb826f06a059
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-16 15:11:41 +0700

    feat(_document): add render page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,27 @@
-import Document, { Html, Main, NextScript, Head } from "next/document";
+import Document, {
+  Html,
+  Main,
+  NextScript,
+  Head,
+  DocumentContext,
+} from "next/document";
 
 class HTMLDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const originalRenderPage = context.renderPage;
+
+    context.renderPage = () => {
+      return originalRenderPage({
+        enhanceApp: (App) => App,
+      });
+    };
+
+    const initialProps = await Document.getInitialProps(context);
+    return {
+      ...initialProps,
+    };
+  }
+
   render() {
     return (
       <Html>
